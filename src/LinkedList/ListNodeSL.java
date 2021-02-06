ee633395d945bc92ba07cbc16aa49a014a9b51d8
siddharth8763 <siddharth8763@gmail.com>
2021-02-06 10:25:11 +0530

    Value equals to index in array added

---
+++ src/LinkedList/ListNodeSL.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class ListNodeSL {
+	 int val;
+	 ListNodeSL next;
+	 ListNodeSL() {}
+	 ListNodeSL(int val) { this.val = val; }
+	 ListNodeSL(int val, ListNodeSL next) { this.val = val; this.next = next; }
+
+}
